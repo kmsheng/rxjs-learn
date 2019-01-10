b41c042edfef8f696691da4a3a27aa74c19778d4
kmsheng <kmsh3ng@gmail.com>
2019-01-10 14:54:11 +0800

    update example of from operator

--- from/example1.js
+++ from/example1.js
@@ -1,4 +1,21 @@
 const { from } = require('rxjs')
 
+// from array
 from([1, 2, 3])
+  .subscribe(console.log)
+
+// from promise
+from(new Promise(resolve => setTimeout(_ => resolve('resolved'), 1000)))
+  .subscribe(console.log)
+
+// from string
+from('string')
+  .subscribe(console.log)
+
+// from Map
+const map = new Map()
+map.set('key1', 'value1')
+map.set('key2', 'value2')
+
+from(map)
   .subscribe(console.log)
